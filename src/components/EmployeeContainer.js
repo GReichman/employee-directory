bf531e3f2e12eeb211a11675e511ec7dce3ef2bd
Garrett Reichman <gsreichman@gmail.com>
2020-04-17 20:07:17 -0400

    added sorting

--- src/components/EmployeeContainer.js
+++ src/components/EmployeeContainer.js
@@ -7,26 +7,67 @@ class EmployeeContainer extends Component{
 
     state = {
         currEmployees: [],
-        allEmployees: []
+        allEmployees: [],
+        sorting: ""
     }
 
     componentDidMount (){
         employees.getEmployees().then(res=>{
             let empList = res.data.results;
+            let empArray = empList.map(element =>{
+                let employee = {
+                    name: element.name.first+" "+ element.name.last,
+                    email: element.email,
+                    phone: element.phone,
+                    pic: element.picture.large,
+                    id: element.id.value
+                }
+                return employee
+            });
             this.setState({
-                currEmployees: getEmpData(empList),
-                allEmployees : empList
+                currEmployees: empArray,
+                allEmployees : empArray
             });
         })
 
+    }
+
+    handleSort = event =>{
+        let property = event.target.name;
+        let order;
+       
+        if(this.state.sorting === property){
+            order = -1;
+            this.setState({
+                sorting: ""
+            });
+        }else{
+            order = 1;
+            this.setState({
+               
+                sorting:property
+            })
+        }
+        console.log(this.state.sorting," ",property)
+        let newArr = sortList(order,this.state.currEmployees,property);
+        this.setState({
+            currEmployees: newArr
+        });
     }
 
     render(){
        
         return(
             <table>
+
                 <tbody>
-               {this.state.currEmployees}
+                <tr>
+                    <th></th>
+                    <th ><button name="name" onClick = {this.handleSort}>Name</button></th>
+                    <th ><button name="phone" onClick ={this.handleSort}>Phone</button></th>
+                    <th ><button name="email" onClick ={this.handleSort}>Email</button></th>
+                </tr>
+               {createEmpRows(this.state.currEmployees)}
                 </tbody>
             </table>
         )
@@ -35,18 +76,23 @@ class EmployeeContainer extends Component{
 
 }
 
-function getEmpData(emps){
-    let empArray = emps.map(element =>{
-        let employee = {
-            name: element.name.first+" "+ element.name.last,
-            email: element.email,
-            phone: element.phone,
-            pic: element.picture.large,
-            id: element.id.value
+function createEmpRows(list){
+return list.map(element =>{
+    return <EmployeeRow name={element.name} email={element.email} phone = {element.phone} pic={element.pic} key={element.id}></EmployeeRow>
+})
+}
+
+function sortList(order, list, property){
+    return list.sort(function(a,b){
+        if(a[property] < b[property]){
+            return -1*order;
+        }else if(a[property] > b[property]){
+            return 1*order;
+        }else{
+            return 0;
         }
-        return <EmployeeRow name={employee.name} email={employee.email} phone = {employee.phone} pic={employee.pic} key={employee.id}></EmployeeRow>
     });
-    return empArray;
+
 }
 
 export default EmployeeContainer;
