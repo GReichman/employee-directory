f3bf225c977beb6a9138a195b77b746642a4080a
Garrett Reichman <gsreichman@gmail.com>
2020-04-18 09:43:13 -0400

    added styling and readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # employee-directory
+Garrett Reichman
 https://greichman.github.io/employee-directory/
+
+## Description
+ This site was made to begin learning React. It uses custom components for the table of employees as well as each employee row. When loaded, the site requests a list of 20 random employees from the randomuser api, parses their data, and displays them all to the page. It then tracks the total list of employees as well as the list of currently displayed employees to manage searching/sorting.
+## Usage
+ To use this app, start by visiting the site linked above. When the page finishes loading, it will display a list of 20 employees. If you wish to sort the list, click either the name, the phone, or the email button at the top (click twice for reverse order). You can also search for an employee using the search bar in the top left.
+
+![screenshot](./public/screenshot.png)
--- src/components/EmployeeContainer.js
+++ src/components/EmployeeContainer.js
@@ -1,9 +1,9 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import EmployeeRow from "./EmployeeRow";
 import employees from "../utils/employees"
 
 
-class EmployeeContainer extends Component{
+class EmployeeContainer extends Component {
 
     state = {
         currEmployees: [],
@@ -11,12 +11,12 @@ class EmployeeContainer extends Component{
         sorting: ""
     }
 
-    componentDidMount (){
-        employees.getEmployees().then(res=>{
+    componentDidMount() {
+        employees.getEmployees().then(res => {
             let empList = res.data.results;
-            let empArray = empList.map(element =>{
+            let empArray = empList.map(element => {
                 let employee = {
-                    name: element.name.first+" "+ element.name.last,
+                    name: element.name.first + " " + element.name.last,
                     email: element.email,
                     phone: element.phone,
                     pic: element.picture.large,
@@ -26,7 +26,7 @@ class EmployeeContainer extends Component{
             });
             this.setState({
                 currEmployees: empArray,
-                allEmployees : empArray
+                allEmployees: empArray
             });
         })
 
@@ -34,48 +34,51 @@ class EmployeeContainer extends Component{
 
     handleSearch = event => {
         let term = event.target.value;
-        const newList = searchList(this.state.allEmployees,term);
+        const newList = searchList(this.state.allEmployees, term);
         this.setState({
-            currEmployees:newList
+            currEmployees: newList
         });
     }
 
-    handleSort = event =>{
+    handleSort = event => {
         let property = event.target.name;
         let order;
-       
-        if(this.state.sorting === property){
+
+        if (this.state.sorting === property) {
             order = -1;
             this.setState({
                 sorting: ""
             });
-        }else{
+        } else {
             order = 1;
             this.setState({
-               
-                sorting:property
+
+                sorting: property
             })
         }
-        console.log(this.state.sorting," ",property)
-        let newArr = sortList(order,this.state.currEmployees,property);
+        console.log(this.state.sorting, " ", property)
+        let newArr = sortList(order, this.state.currEmployees, property);
         this.setState({
             currEmployees: newArr
         });
     }
 
-    render(){
-       
-        return(
-            <table>
-                <tbody>
+    render() {
+
+        return (
+            <table className="table table-striped table-bordered table-hover">
+                <thead className="thead-dark">
                 <tr>
-                    <th></th>
-                    <th ><button name="name" onClick = {this.handleSort}>Name</button></th>
-                    <th ><button name="phone" onClick ={this.handleSort}>Phone</button></th>
-                    <th ><button name="email" onClick ={this.handleSort}>Email</button></th>
-                    <th><input type="text" onChange = {this.handleSearch} placeholder="Search"></input></th>
-                </tr>
-               {createEmpRows(this.state.currEmployees)}
+                        <th ><input type="text" onChange={this.handleSearch} placeholder="Search"></input></th>
+                        <th ><button className = "btn btn-light" name="name" onClick={this.handleSort}>Name</button></th>
+                        <th ><button className = "btn btn-light" name="phone" onClick={this.handleSort}>Phone</button></th>
+                        <th ><button className = "btn btn-light" name="email" onClick={this.handleSort}>Email</button></th>
+
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {createEmpRows(this.state.currEmployees)}
                 </tbody>
             </table>
         )
@@ -84,30 +87,30 @@ class EmployeeContainer extends Component{
 
 }
 
-function createEmpRows(list){
-return list.map(element =>{
-    return <EmployeeRow name={element.name} email={element.email} phone = {element.phone} pic={element.pic} key={element.id}></EmployeeRow>
-})
+function createEmpRows(list) {
+    return list.map(element => {
+        return <EmployeeRow name={element.name} email={element.email} phone={element.phone} pic={element.pic} key={element.id}></EmployeeRow>
+    })
 }
 
-function sortList(order, list, property){
-    return list.sort(function(a,b){
-        if(a[property] < b[property]){
-            return -1*order;
-        }else if(a[property] > b[property]){
-            return 1*order;
-        }else{
+function sortList(order, list, property) {
+    return list.sort(function (a, b) {
+        if (a[property] < b[property]) {
+            return -1 * order;
+        } else if (a[property] > b[property]) {
+            return 1 * order;
+        } else {
             return 0;
         }
     });
 
 }
 
-function searchList(list, term){
+function searchList(list, term) {
     return list.filter(emp => {
-        if(emp.name.toUpperCase().includes(term.toUpperCase())){
+        if (emp.name.toUpperCase().includes(term.toUpperCase())) {
             return true;
-        }else{
+        } else {
             return false;
         }
     })
